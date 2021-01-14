38e21178242dedc4c7e350fc2ef1b1522cc1b4fb
Jeremy Langevin <langevinj832@gmail.com>
2021-01-14 09:45:31 -0500

    Bubblesort implemented

--- bubble.js
+++ bubble.js
@@ -1,3 +1,18 @@
-function bubbleSort() {}
+function bubbleSort(arr=[]) {
+    //base case for returing an empty array
+    if(!arr.length) return arr;
+    let i = arr.length;
+
+    //loop through array based on length and what has been sorted
+    while(i > 0){
+        for(let j = 0; j < i - 1; j++){
+            //if the starting element is bigger then the next, swap
+            if(arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        }
+        i--;
+    }
+
+    return arr;
+}
 
 module.exports = bubbleSort;
