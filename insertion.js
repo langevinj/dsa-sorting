41b3534a032a46bb8f41dbac5c14d150ee084785
Jeremy Langevin <langevinj832@gmail.com>
2021-01-14 10:49:57 -0500

    Implemented insertionSort

--- insertion.js
+++ insertion.js
@@ -1,3 +1,25 @@
-function insertionSort() {}
+function insertionSort(arr=[]) {
+    if(!arr.length) return arr;
+
+    let l = arr.length;
+    let comp = arr[1];
+
+    if(comp < arr[0]){
+        [arr[0], arr[1]] = [arr[1], arr[0]]
+    }
+
+    let i = 2;
+
+    while(i < l){
+        if(arr[i - 1] > arr[i]){
+            for(let j = i; j > 0; j--){
+                if(arr[j] < arr[j - 1]) [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+            }
+        }
+        i++
+    }
+
+    return arr
+}
 
 module.exports = insertionSort;
