96ba9b1eb78c7e813b4dcf287076c75e0698a3cf
Jeremy Langevin <langevinj832@gmail.com>
2021-01-14 12:16:32 -0500

    Quicksort implemented

--- quick.js
+++ quick.js
@@ -4,6 +4,7 @@ You can assume the pivot is always the first element
 */
 
 function pivot(arr, start=0, end=arr.length - 1){
+    if(arr.length <= 1) return [arr, null]
     let piv = arr[start];
     let pIdx = 0;
 
@@ -23,6 +24,34 @@ function pivot(arr, start=0, end=arr.length - 1){
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr) {
+    if(arr.length <= 1) return arr || []
 
-module.exports = { pivot, quickSort};
+    function pivot(arr, start = 0, end = arr.length - 1) {
+        if (arr.length <= 1) return [arr, null]
+        let piv = arr[start];
+        let pIdx = 0;
+
+        while (start <= end) {
+            if (arr[start] < piv) {
+                pIdx++;
+                [arr[start], arr[pIdx]] = [arr[pIdx], arr[start]];
+            }
+            start++
+        }
+
+        [arr[0], arr[pIdx]] = [arr[pIdx], arr[0]]
+        return [arr, pIdx]
+    }
+
+    let res = pivot(arr);
+    let arrM = res[0];
+    let pIdx = res[1];
+
+    let left = [...quickSort(arrM.slice(0, pIdx))];
+    let right = [arrM[pIdx], ...quickSort(arrM.slice(pIdx + 1))]
+
+    return [...left, ...right]
+}
+
+module.exports = { pivot, quickSort };
