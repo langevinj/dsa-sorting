6ca3dbf9d0bfd002d41d2ee28a9af4e30e9e31f5
Jeremy Langevin <langevinj832@gmail.com>
2021-01-14 13:44:39 -0500

    radixSort completed

--- radix.js
+++ radix.js
@@ -22,6 +22,22 @@ function mostDigits(arr){
 
 
 function radixSort(arr) {
+    //find the most amount of digits in one element of the array
+    let max = mostDigits(arr);
+    let x = 0;
+
+    while(x <= max){
+        for(let i = 1; i < arr.length; i++){
+            for(let j = i; j > 0; j --){
+                if(getDigit(arr[j], x) < getDigit(arr[j - 1], x)){
+                    [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+                }
+            }
+        }
+        x++
+    }
+    return arr
 }
 
+radixSort([8, 6, 1, 12])
 module.exports = { getDigit, digitCount, mostDigits, radixSort};
