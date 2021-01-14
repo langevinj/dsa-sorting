3be17ac833a256e454edc95372e722ffdb6c9ef8
Jeremy Langevin <langevinj832@gmail.com>
2021-01-14 13:23:29 -0500

    getDigit helper written?

--- radix.js
+++ radix.js
@@ -1,3 +1,20 @@
-function radixSort() {}
+function getDigit(num, place){
+    let numString = num.toString();
+    
+    return place > numString.length - 1 ? 0 : +(numString[numString.length - place - 1])
+}
 
-module.exports = radixSort;
+function digitCount(){
+
+}
+
+function mostDigits(){
+
+}
+
+
+
+function radixSort(arr) {
+}
+
+module.exports = { getDigit, digitCount, mostDigits, radixSort};
